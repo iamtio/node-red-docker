6e21f269229ca1e5fa184b234069a1c1b077f103
Igor Tsarev <9735058@gmail.com>
2021-03-13 14:59:42 +0300

    Added alsa

--- docker-custom/docker-alpine.sh
+++ docker-custom/docker-alpine.sh
@@ -6,11 +6,11 @@ echo "node-red version: ${NODE_RED_VERSION}"
 echo "#########################################################################"
 
 docker build --rm --no-cache \
-    --build-arg ARCH=amd64 \
+    --build-arg ARCH=arm32v7 \
     --build-arg NODE_VERSION=12 \
     --build-arg NODE_RED_VERSION=${NODE_RED_VERSION} \
     --build-arg OS=alpine \
     --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
     --build-arg TAG_SUFFIX=default \
     --file Dockerfile.custom \
-    --tag testing:node-red-build .
+    --tag iamtio/node-red:1.2.9 .
